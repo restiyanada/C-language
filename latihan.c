2d89d6ff6a6b34b1b661035c887f04fcd4cf67bc
Restiyana Dwi Astuti <restiyana.dwi@gmail.com>
2016-07-25 11:54:39 +0700

    versi 1.0

---
+++ latihan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+int y=10;
+int x=8
+int *pointer_x;
+pointer_x= &x;
+printf("pointer x = %x\n", pointer_x);
+printf("*pointer x = %d\n, *pointer");
+printf("selesai\n");
+
+return 0;
+}
